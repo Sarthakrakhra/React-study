56321301f244dca5370072ed70291c34d60a6dee
Sarthak Rakhra <sr691312@dal.ca>
2020-05-14 19:57:27 -0300

    Render content conditionally code updated to work

--- render-content-conditionally/src/App.js
+++ render-content-conditionally/src/App.js
@@ -1,78 +1,75 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
-
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    persons : [
-      {name: "Max", age: 28},
-      {name: "Manu", age: 29},
-      {name: "Stephanie", age: 26}
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
     ],
     otherState: "some other value",
-  }
+    showPersons: false,
+  };
 
-  switchNameHandler = (newName) =>{
-    //DONT DO THIS: this.state.persons[0].name="Maximilian";
-
-    this.setState({
-      persons :[
-        {name: newName, age: 28},
-        {name: "Manu", age: 29},
-        {name: "Stephanie", age: 27}
-      ]
-    })
-  }
+  togglePersonDiv = () => {
+    const showPersonsDiv = this.state.showPersons;
+    this.setState({ showPersons: !showPersonsDiv });
+  };
 
   nameChangedHandler = (event) => {
     this.setState({
-      persons :[
-        {name: 'Max', age: 28},
-        {name: event.target.value, age: 29},
-        {name: "Stephanie", age: 27}
-      ]
-    })
-  }
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 27 },
+      ],
+    });
+  };
 
-  render () {
-
+  render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherint',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      backgroundColor: "white",
+      font: "inherint",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
     };
 
+    let persons = null; //persons variable is by default set to null so that the render function does not need to render elements if the state of showPersons === false
+
+    // if the showPersons state === true then we assign the variable persons to the JSX div containing the Person components
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            changed={this.nameChangedHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
     return (
       <div className="App">
-         <h1> Hi, I'm a React App </h1>
-         <p>This is really working!</p>
-         <button 
-            onClick={() => this.switchNameHandler('Maximillian!!')} 
-            style={style}
-          >
-            Switch name
-          </button>
-
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-        />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')} 
-          changed={this.nameChangedHandler}
-        >
-            My Hobbies: Racing
-        </Person>
-        
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}
-        />
+        <h1> Hi, I'm a React App </h1>
+        <p>This is really working!</p>
+        <button onClick={this.togglePersonDiv} style={style}>
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     );
   }
@@ -80,61 +77,36 @@ class App extends Component {
 
 export default App;
 
+/*
 
-//FUNCTIONAL WAY OF DOING IT 
-//  |
-//  |
-//  |
-//  V
-
-// const app = (props) =>{
-
-//     const [this.state, setthis.state] = useState({
-//       persons : [
-//         {name: "Max", age: 28},
-//         {name: "Manu", age: 29},
-//         {name: "Stephanie", age: 26}
-//       ]
-//     });
-
-//     const [otherState, setOtherState] = useState('some other value')
-
-//     const switchNameHandler = (newName) =>{
-//       //DONT DO THIS: this.state.persons[0].name="Maximilian";
-
-//       setthis.state({
-//         persons :[
-//           {name: newName, age: 28},
-//           {name: "Manu", age: 29},
-//           {name: "Stephanie", age: 27}
-//         ]
-//       })
-//     }
+One way of rendering content conditionally inside of the JSX code can be by encapsulating the Person components inside a div 
+and further encapsulating that inside a ternary expression. The ternary expression is checking if the showPersons state is equal to true.
+If yes, then we render the persons div, else we don't render anything
 
 
-//     return (
-//       <div className="App">
-//         <h1> Hi, I'm a React App </h1>
-//         <p>This is really working!</p>
-//         <button onClick={switchNameHandler.bind(this, 'Maximillian')}>Switch name</button>
-
-//         <Person 
-//           name={this.state.persons[0].name} 
-//           age={this.state.persons[0].age}/>
-//         <Person 
-//           name={this.state.persons[1].name} 
-//           age={this.state.persons[1].age}
-//           click={switchNameHandler.bind(this, 'Max!')} >
-//             My Hobbies: Racing
-//         </Person>
-//         <Person 
-//           name={this.state.persons[2].name} 
-//           age={this.state.persons[2].age}/>
-//       </div>
+{
+  this.state.showPersons ?
+    <div>
+      <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+      />
+      <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Max!')} 
+        changed={this.nameChangedHandler}
+      >
+          My Hobbies: Racing
+      </Person>
       
-//     );
+      <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}
+      />
+    </div> : null
+}
 
-//     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
-// }
 
-// export default app;
+
+*/
